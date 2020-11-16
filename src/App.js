955b153daacad4cef23037a787490bc7a47933c2
IMDAD <ahmedimdad1593@gmail.com>
2020-11-16 15:10:47 +0600

    redux integrate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import logo from './logo.svg';
-import './App.scss';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import HomePage from './components/Home/HomePage/HomePage';
+import logo from "./logo.svg";
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./components/Home/HomePage/HomePage";
+import Bookings from "./components/Bookings/Bookings";
 function App() {
   return (
     <Router>
       <Switch>
-      <Route path="/home"> 
+        <Route path="/home">
           <HomePage></HomePage>
-      </Route>
+        </Route>
+        <Route path="/login">
+          <h1>Login</h1>
+        </Route>
+        <Route path="/details">
+          <h1>Details</h1>
+        </Route>
+        <Route path="/bookings">
+          <Bookings></Bookings>
+        </Route>
         <Route exact path="/">
-        <HomePage></HomePage>
+          <HomePage></HomePage>
         </Route>
-        <Route path="*">
-          
-        </Route>
-
+        <Route path="*"></Route>
       </Switch>
     </Router>
   );
--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -5,25 +5,36 @@ import banner from "../../../Images/Rectangle 13.png";
 const Banner = () => {
   return (
     <section className="banner">
-      <div className="row">
-   
-              <div className="col-md-6 offset-md-3 ">
-                  <div className="row">
-                  <h1 className="text-white" style={{textAlign:"center", margin:"0 auto" , marginTop:"150px"}}>Find Your House Rent</h1>
-                  </div>
-            <div className="row">
-              <dib className="col-md-8">
-              <input type="text" className="form-control banner-item" placeholder="search..." />
-              </dib>
-              <div className="col-md-4 ">
+      <div className="row" style={{ marginRight: "0px" }}>
+        <div className="col-md-6 offset-md-3 ">
+          <div className="row">
+            <h1
+              className="text-white"
+              style={{
+                textAlign: "center",
+                margin: "0 auto",
+                marginTop: "150px",
+              }}
+            >
+              Find Your House Rent
+            </h1>
+          </div>
+          <div className="row">
+            <dib className="col-md-8">
+              <input
+                type="text"
+                className="form-control banner-item"
+                placeholder="search..."
+              />
+            </dib>
+            <div className="col-md-4 ">
               <button type="button" className=" banner-btn">
-              Find
-            </button>
-              </div>
+                Find
+              </button>
             </div>
           </div>
         </div>
-     
+      </div>
     </section>
   );
 };
