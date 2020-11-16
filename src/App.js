ff84fc25a0c49220256537bb5d27eaf00a617c1b
JahirulKaiyoumd <imdjahirulislam76@gmail.como.com>
2020-11-16 17:06:39 +0600

    homepage and details page updated

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import {
   Link
 } from "react-router-dom";
 import HomePage from './components/Home/HomePage/HomePage';
+import ApartmentDetails from './components/ApartmentDetails/ApartmentDetails';
 function App() {
   return (
     <Router>
       <Switch>
       <Route path="/home"> 
           <HomePage></HomePage>
-      </Route>
+        </Route>
+        <Route path="/aparmentDetails/:appId">
+        <ApartmentDetails></ApartmentDetails>
+        </Route>
         <Route exact path="/">
         <HomePage></HomePage>
         </Route>
--- src/components/Home/homepageApartment/HomePageApartment.js
+++ src/components/Home/homepageApartment/HomePageApartment.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Button, Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarker, faBed, faBath } from "@fortawesome/free-solid-svg-icons";
+import { useHistory } from "react-router-dom";
 
 const HomePageApartment = (props) => {
+
+  const history = useHistory();
   const {
     serviceTitle,
     price,
@@ -11,7 +14,12 @@ const HomePageApartment = (props) => {
     noOfBathRoom,
     noOfBedRoom,
     thumbnail,
+    id
   } = props.apartment;
+
+  const seeDetails = () => {
+    history.push(`/aparmentDetails/${id}`)
+  }
 
   return (
     <div className="col-md-4" style={{marginTop:"20px"}}>
@@ -40,7 +48,7 @@ const HomePageApartment = (props) => {
                       <h3>{price}</h3>
                       </div>
             <div className="col-md-7">
-              <Button variant="primary">View Details</Button>
+              <Button variant="primary" onClick={seeDetails}>View Details</Button>
             </div>
           </div>
         </Card.Body>
